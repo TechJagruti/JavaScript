cd26321f5a8cb461b8f5dbd0c0f7279f739bbc26
TechJagruti <techjagruti@gmail.com>
2026-02-25 15:12:28 +0530

    Update SingleLineComments.js

--- SingleLineComments.js
+++ SingleLineComments.js
@@ -1,6 +1,4 @@
 <Style>
-document.write("<h3>If You Show Console log follow below Steps --- </h3>");
-Document.write("Right Click → Inspect → Console");
 console.log("Dekh lo bhai, Yeh to dikh jayega");
 //console.log("lekin, Yeh to nahi dikhekha");
 </Style>
