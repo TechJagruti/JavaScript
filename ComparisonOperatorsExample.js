ad9a5a80e37f19a4c3a7d88af0236a3e0aff6683
TechJagruti <techjagruti@gmail.com>
2026-03-02 16:05:04 +0530

    Update and rename ComparisonOperatorsExample.html to ComparisonOperatorsExample.js

---
+++ ComparisonOperatorsExample.js
@@ -0,0 +1,18 @@
+
+       let a = 10;
+let b = 5;
+let c = "10";
+
+console.log("Values:");
+console.log("a = " + a);
+console.log("b = " + b);
+console.log("c = " + c);
+
+console.log("a == c : " + (a == c));
+console.log("a === c : " + (a === c));
+console.log("a != b : " + (a != b));
+console.log("a !== c : " + (a !== c));
+console.log("a > b : " + (a > b));
+console.log("a < b : " + (a < b));
+console.log("a >= 10 : " + (a >= 10));
+console.log("b <= 5 : " + (b <= 5));
