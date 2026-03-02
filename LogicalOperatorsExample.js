f6f4f2b8aa7c02f0239c53a811a7a71171324b86
TechJagruti <techjagruti@gmail.com>
2026-03-02 16:07:22 +0530

    Update and rename LogicalOperatorsExample.html to LogicalOperatorsExample.js

---
+++ LogicalOperatorsExample.js
@@ -0,0 +1,18 @@
+        let a = 10;
+        let b = 5;
+
+        console.log("Values:");
+        console.log("a = " + a);
+        console.log("b = " + b);
+
+        // AND (&&)
+        console.log("(a > 5 && b < 10) : " + (a > 5 && b < 10));
+        console.log("(a < 5 && b < 10) : " + (a < 5 && b < 10));
+
+        // OR (||)
+        console.log("(a < 5 || b < 10) : " + (a < 5 || b < 10));
+        console.log("(a < 5 || b > 10) : " + (a < 5 || b > 10));
+
+        // NOT (!)
+        console.log("!(a > b) : " + !(a > b));
+        console.log("!(a < b) : " + !(a < b));
