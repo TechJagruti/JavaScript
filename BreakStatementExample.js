4b9144d59ea1e5e19aef68532a4e9414dcebeee3
TechJagruti <techjagruti@gmail.com>
2026-03-02 16:35:43 +0530

    Update and rename BreakStatementExample.html to BreakStatementExample.js

---
+++ BreakStatementExample.js
@@ -0,0 +1,9 @@
+
+    for (let i = 1; i <= 10; i++) {
+
+        if (i === 5) {
+            break;   // Stop loop when i becomes 5
+        }
+
+        console.log("Number: " + i + "<br>");
+    }
