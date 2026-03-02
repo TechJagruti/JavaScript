877b74e5356d7f03e349e080b1572b037e037c01
TechJagruti <techjagruti@gmail.com>
2026-03-02 15:53:36 +0530

    Update and rename ArithmeticOperatorsExample.html to ArithmeticOperatorsExample.js

---
+++ ArithmeticOperatorsExample.js
@@ -0,0 +1,34 @@
+
+        let a = 10;
+        let b = 3;
+
+        console.log("<h3>Values:</h3>");
+        console.log("a = " + a + "<br>");
+        console.log("b = " + b + "<br><br>");
+
+        // Addition
+        console.log("Addition (a + b) = " + (a + b) + "<br>");
+
+        // Subtraction
+        console.log("Subtraction (a - b) = " + (a - b) + "<br>");
+
+        // Multiplication
+        console.log("Multiplication (a * b) = " + (a * b) + "<br>");
+
+        // Division
+        console.log("Division (a / b) = " + (a / b) + "<br>");
+
+        // Modulus
+        console.log("Modulus (a % b) = " + (a % b) + "<br>");
+
+        // Exponentiation
+        console.log("Exponent (a ** b) = " + (a ** b) + "<br><br>");
+
+        // Increment
+        a++;
+        console.log("After Increment (a++) = " + a + "<br>");
+
+        // Decrement
+        b--;
+        console.log("After Decrement (b--) = " + b + "<br>");
+   
