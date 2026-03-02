93774472d4d3ca185ba59ca899c36bc68c69e4fd
TechJagruti <techjagruti@gmail.com>
2026-03-02 16:14:29 +0530

    Update and rename IncrementDecrementExample.html to IncrementDecrementExample.js

---
+++ IncrementDecrementExample.js
@@ -0,0 +1,20 @@
+let x = 5;
+
+console.log("Initial Value:");
+console.log("x = " + x);
+
+// Post-Increment
+console.log("Post-Increment (x++) → " + (x++));
+console.log("Value of x after x++ → " + x);
+
+// Pre-Increment
+console.log("Pre-Increment (++x) → " + (++x));
+console.log("Value of x after ++x → " + x);
+
+// Post-Decrement
+console.log("Post-Decrement (x--) → " + (x--));
+console.log("Value of x after x-- → " + x);
+
+// Pre-Decrement
+console.log("Pre-Decrement (--x) → " + (--x));
+console.log("Value of x after --x → " + x);
