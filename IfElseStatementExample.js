09b4d84fa2928b0fc974a7a06520c3f36954f3cc
TechJagruti <techjagruti@gmail.com>
2026-03-10 16:31:40 +0530

    Update IfElseStatementExample.js

--- IfElseStatementExample.js
+++ IfElseStatementExample.js
@@ -1,8 +1,6 @@
-
-        let age = 16;
-
-        if (age >= 18) {
-            document.write("You are eligible to vote.");
-        } else {
-            document.write("You are NOT eligible to vote.");
-        }
+let age = 16;
+if (age >= 18) {
+        console.log("You are eligible to vote.");
+} else {
+        console.log("You are NOT eligible to vote.");
+}
