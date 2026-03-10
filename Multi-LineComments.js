0f389d50a35c9bf26bf77686a393fe996f0c0c56
TechJagruti <techjagruti@gmail.com>
2026-03-10 16:21:01 +0530

    Update Multi-LineComments.js

--- Multi-LineComments.js
+++ Multi-LineComments.js
@@ -1,4 +1,4 @@
-document.write("This is Multi-Line Comments");
+console.log("This is Multi-Line Comments");
 console.log("Dekh lo bhai, Yeh to dikh jayega");
 /*console.log("lekin, Yeh to nahi dikhekha");
 console.log("Aur, Yeh bhi nahi dikhekha");*/
